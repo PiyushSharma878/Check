fe8531927176da9dca3a567f846ef5b1170282ff
unknown <piysh878sharma@gmail.com>
2025-01-21 21:19:02 +0530

    testing

---
+++ EvenNo.js
@@ -0,0 +1,6 @@
+//even no btw 1 to 15
+let n=15;
+for(let i=2;i<=n;i=i+2){
+    console.log("even",i)
+}
+    
---
+++ oddNumber.js
@@ -0,0 +1,10 @@
+//odd no btw 1 to 15
+let n=15;
+for(let i=2;i<=n;i=i+2){
+    console.log("odd no between 15",i);
+}
+// // for backward orreversec
+// let n=15;
+// for(let i=15;i<=2;i=i-2){
+//     console.log(i)
+//}
